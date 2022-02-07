aa7ab6fe3d493ea24960440b017f7c1792a93ce5
p-vascool <vasko_pan1@mail.bg>
2022-02-07 23:42:10 +0200

    Api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response=>{
+    axios.get('http://localhost:5000/api/Activities').then(response=>{
       console.log(response);
       setActivities(response.data);
     })
