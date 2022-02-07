132d452e143b83ed3ef732e66ae3267dbc51e5e2
p-vascool <vasko_pan1@mail.bg>
2022-02-07 23:11:52 +0200

    Added client app - React with TS

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,18 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000")
+            .WithOrigins("http://localhost:3001");
+    }
+    );
+});
 var app = builder.Build();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -38,7 +49,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
+app.UseCors("CorsPolicy");
 // app.UseHttpsRedirection();
 
 app.UseAuthorization();
